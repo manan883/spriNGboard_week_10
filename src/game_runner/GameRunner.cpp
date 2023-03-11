2d9fa6abd0bfa878c20c8cb8c42690da1835c9eb
kevin_workspace <kevin@aws_workspace>
2023-03-10 20:35:01 -0800

    add bulk regression tests

---
+++ src/game_runner/GameRunner.cpp
@@ -0,0 +1,37 @@
+﻿#include <stdlib.h>
+#include "Game.h"
+
+static bool notAWinner;
+
+int main(int argc, char** argv)
+{
+  if (argc == 1) {
+    srand(time(NULL));
+  }
+  else {
+    unsigned seed = stoi(argv[1]);
+    srand(seed);
+  }
+
+	Game aGame;
+
+	aGame.add("Chet");
+	aGame.add("Pat");
+	aGame.add("Sue");
+
+	do
+	{
+
+		aGame.roll(rand() % 5 + 1);
+
+		if (rand() % 9 == 7)
+		{
+			notAWinner = aGame.wrongAnswer();
+		}
+		else
+		{
+			notAWinner = aGame.wasCorrectlyAnswered();
+		}
+	} while (notAWinner);
+
+}
