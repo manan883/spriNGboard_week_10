f0c14d03bcfc00ebb90ddb2946057773812f1439
manan <manan@aws.com>
2023-03-30 21:27:28 -0700

    built up unit tests to test scoring and built some methods to make it work, no logic yet

--- src/bowling_lib/bowling.h
+++ src/bowling_lib/bowling.h
@@ -1,14 +1,29 @@
 #pragma once
-
+#include <vector>
 namespace Bowling
 {
   class Game
 {
   public:
-    Game(){}
-    bool isComplete(){return true;}
-    void recordBall(int num_pins);
-    int getScore();
+    bool g_isComplete;
+    int score;
+    std::vector<int> balls;
+    Game()
+    {
+      g_isComplete = false;
+      score = 0;
+      balls = {};
+    }
+    void setGameStatus(bool status){g_isComplete = status;}
+    bool isComplete(){return g_isComplete;}
+    void recordBall(int num_pins)
+    {
+      balls.push_back(num_pins);
+    }
+    void calcScore(){
+      score = 30;
+    }
+    int getScore(){return score;}
 };
  
 }
--- test/bowling_lib/test__Bowling.cc
+++ test/bowling_lib/test__Bowling.cc
@@ -5,6 +5,24 @@
 TEST_CASE ("this test should pass")
 {
   Bowling::Game g;
-  REQUIRE(g.isComplete() == true);
+    g.setGameStatus(false);
+    WHEN("num_pins for a rolls knocked down is 10")
+    {
+      g.resetBalls();
+      g.recordBall(10);
+      g.recordBall(10);
+      g.calcScore();
+      REQUIRE(g.getScore() == 30);
+    }
+    WHEN("pins is 5 and 5")
+    {
+      g.resetBalls();
+      g.recordBall(5);
+      g.recordBall(5);
+      g.recordBall(3);
+      g.calcScore();
+      REQUIRE(g.getScore() == 13);
+
+    }
 }
 
